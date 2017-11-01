6be0a3270dd6aab1f018b7e26f8b027316bf6b58
Yangmu Jiang <celsius.j@gmail.com>
2017-10-31 21:47:42 -0700

    Test: parseProgramAndGetCode -> executeProgramAndGetExitCode

--- test/ir_test.cpp
+++ test/ir_test.cpp
@@ -10,7 +10,7 @@ class IRTester : public Tester
     virtual void setUp() {}
     virtual void shutDown() {}
 
-    int parseProgramAndGetCode(const char *prog, std::string *msg = nullptr)
+    int executeProgramAndGetExitCode(const char *prog, std::string *msg = nullptr)
     {
         IRSimulator simu;
         int ret = 0;
@@ -54,82 +54,82 @@ class IRTester : public Tester
 TEST_F(IRTester, BasicExpression)
 {
     // comma
-    EXPECT_EQ(49, parseProgramAndGetCode("int main() { return 2,3,11,49; }"));
+    EXPECT_EQ(49, executeProgramAndGetExitCode("int main() { return 2,3,11,49; }"));
 
     // assign
     EXPECT_EQ(
-        49, parseProgramAndGetCode("int main() { int a; a = 49; return a; }"));
+        49, executeProgramAndGetExitCode("int main() { int a; a = 49; return a; }"));
 
     // cond
-    EXPECT_EQ(49, parseProgramAndGetCode(
+    EXPECT_EQ(49, executeProgramAndGetExitCode(
                       "int main() { int a; a = 1 ? 49 : 22; return a; }"));
-    EXPECT_EQ(49, parseProgramAndGetCode(
+    EXPECT_EQ(49, executeProgramAndGetExitCode(
                       "int main() { int a; a = 0 ? 22 : 49; return a; }"));
 
     // logical
     // TODO: short-circuit
     std::string msg;
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 49 || 0; }", &msg), msg);
+        1, executeProgramAndGetExitCode("int main() { return 49 || 0; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 0 || 49; }", &msg), msg);
+        1, executeProgramAndGetExitCode("int main() { return 0 || 49; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        0, parseProgramAndGetCode("int main() { return 0 || 0; }", &msg), msg);
+        0, executeProgramAndGetExitCode("int main() { return 0 || 0; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 49 && 49; }", &msg),
+        1, executeProgramAndGetExitCode("int main() { return 49 && 49; }", &msg),
         msg);
     EXPECT_EQ_PRINT(
-        0, parseProgramAndGetCode("int main() { return 0 && 49; }", &msg), msg);
+        0, executeProgramAndGetExitCode("int main() { return 0 && 49; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        0, parseProgramAndGetCode("int main() { return 49 && 0; }", &msg), msg);
+        0, executeProgramAndGetExitCode("int main() { return 49 && 0; }", &msg), msg);
 
     // bit-wise
     EXPECT_EQ_PRINT(
-        7, parseProgramAndGetCode("int main() { return 5 | 3; }", &msg), msg);
+        7, executeProgramAndGetExitCode("int main() { return 5 | 3; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 5 & 3; }", &msg), msg);
+        1, executeProgramAndGetExitCode("int main() { return 5 & 3; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        6, parseProgramAndGetCode("int main() { return 5 ^ 3; }", &msg), msg);
+        6, executeProgramAndGetExitCode("int main() { return 5 ^ 3; }", &msg), msg);
 
     // equality
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 49 == 49; }", &msg), msg);
+        1, executeProgramAndGetExitCode("int main() { return 49 == 49; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        0, parseProgramAndGetCode("int main() { return 49 == 47; }", &msg), msg);
+        0, executeProgramAndGetExitCode("int main() { return 49 == 47; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        0, parseProgramAndGetCode("int main() { return 49 != 49; }", &msg), msg);
+        0, executeProgramAndGetExitCode("int main() { return 49 != 49; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 49 != 47; }", &msg), msg);
+        1, executeProgramAndGetExitCode("int main() { return 49 != 47; }", &msg), msg);
 
     // relation
     EXPECT_EQ_PRINT(
-        0, parseProgramAndGetCode("int main() { return 49 < 47; }", &msg), msg);
+        0, executeProgramAndGetExitCode("int main() { return 49 < 47; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        0, parseProgramAndGetCode("int main() { return 49 <= 47; }", &msg), msg);
+        0, executeProgramAndGetExitCode("int main() { return 49 <= 47; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 49 > 47; }", &msg), msg);
+        1, executeProgramAndGetExitCode("int main() { return 49 > 47; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        1, parseProgramAndGetCode("int main() { return 49 >= 47; }", &msg), msg);
+        1, executeProgramAndGetExitCode("int main() { return 49 >= 47; }", &msg), msg);
 
     // shift
     EXPECT_EQ_PRINT(
-        14, parseProgramAndGetCode("int main() { return 7 << 1; }", &msg), msg);
+        14, executeProgramAndGetExitCode("int main() { return 7 << 1; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        3, parseProgramAndGetCode("int main() { return 7 >> 1; }", &msg), msg);
+        3, executeProgramAndGetExitCode("int main() { return 7 >> 1; }", &msg), msg);
 
     // additive
     EXPECT_EQ_PRINT(
-        12, parseProgramAndGetCode("int main() { return 7 + 5; }", &msg), msg);
+        12, executeProgramAndGetExitCode("int main() { return 7 + 5; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        2, parseProgramAndGetCode("int main() { return 7 - 5; }", &msg), msg);
+        2, executeProgramAndGetExitCode("int main() { return 7 - 5; }", &msg), msg);
 
     // multiplicative
     EXPECT_EQ_PRINT(
-        35, parseProgramAndGetCode("int main() { return 7 * 5; }", &msg), msg);
+        35, executeProgramAndGetExitCode("int main() { return 7 * 5; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        7, parseProgramAndGetCode("int main() { return 35 / 5; }", &msg), msg);
+        7, executeProgramAndGetExitCode("int main() { return 35 / 5; }", &msg), msg);
     EXPECT_EQ_PRINT(
-        2, parseProgramAndGetCode("int main() { return 7 % 5; }", &msg), msg);
+        2, executeProgramAndGetExitCode("int main() { return 7 % 5; }", &msg), msg);
 
 }
 // statement
