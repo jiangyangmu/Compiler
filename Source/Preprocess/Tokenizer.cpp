1fb4a314ded371df21c35ae2b0e8f64862422d34
Yangmu Jiang <celsius.j@gmail.com>
2019-08-16 01:20:01 -0700

    tokenizer

---
+++ Source/Preprocess/Tokenizer.cpp
@@ -0,0 +1,551 @@
+#include "Tokenizer.h"
+
+#include <cctype>
+#include <exception>
+#include <unordered_map>
+#include <xutility>
+
+const char * ParseId(const char * pcBegin);
+const char * ParseNumericConstant(const char * pcBegin);
+const char * ParseCharConstant(const char * pcBegin);
+const char * ParseStringLiteral(const char * pcBegin);
+const char * ParseOperatorPunctuator(const char * pcBegin);
+
+const char * ParsePPDirective(const char * pcBegin);
+const char * ParsePPIncludedFilePath(const char * pcBegin);
+const char * ParsePPLParan(const char * pcBegin);
+
+static inline bool isoctdigit(int c) {
+    return c >= '0' && c <= '7';
+}
+
+// \' \" \? \\ \a \b \f \n \r \t \v
+static inline bool isescape(int c) {
+    bool escape = false;
+    switch (c)
+    {
+        case '\'':
+        case '"':
+        case '?':
+        case '\\':
+        case 'a':
+        case 'b':
+        case 'f':
+        case 'n':
+        case 'r':
+        case 't':
+        case 'v':
+            escape = true;
+        default:
+            break;
+    }
+    return escape;
+}
+
+struct TokenRecog {
+    enum Category {
+        UNKNOWN,
+        ID,
+        FLOAT,
+        INT,
+        CHAR,
+        STRING,
+        OP_OR_PUNC,
+    };
+
+    struct Info {
+        Category category;
+
+        Info()
+            : category(UNKNOWN) {
+        }
+    };
+};
+
+// Stop at the first non-space character.
+static inline const char * TokenStart(const char * p) {
+    while (isspace(*p))
+        ++p;
+    return p;
+}
+
+// Stop at the first character after current "id", or start if not match.
+static inline const char * IdEnd(const char * start) {
+    if (*start == '_' || isalpha(*start))
+    {
+        ++start;
+        while (*start == '_' || isalnum(*start))
+            ++start;
+    }
+    return start;
+}
+
+// Stop at the first character after current "float", or start if not match.
+static inline const char * FloatConstEnd(const char * start) {
+    const char * end = start;
+
+    // fractional constant
+    bool has_dot = false;
+    bool has_digit = false;
+    while (isdigit(*end))
+        ++end, has_digit = true;
+    if (*end == '.')
+        ++end, has_dot = true;
+    while (isdigit(*end))
+        ++end, has_digit = true;
+    if (!has_digit)
+        return start;
+
+    // exponent part
+    if (!has_dot && tolower(*end) != 'e')
+        return start;
+    if (tolower(*end) == 'e')
+    {
+        ++end;
+        if (*end == '+' || *end == '-')
+            ++end;
+        while (isdigit(*end))
+            ++end;
+    }
+
+    // floating suffix
+    if (tolower(*end) == 'f' || tolower(*end) == 'l')
+        ++end;
+
+    return end;
+}
+
+// Stop at the first character after current "int", or start if not match.
+static inline const char * IntConstEnd(const char * start) {
+    const char * end = start;
+
+    if (*end >= '0' && *end <= '9') // decimal constant
+    {
+        ++end;
+        while (isdigit(*end))
+            ++end;
+    }
+    else if (*end == '0' && tolower(*(end + 1)) != 'x') // octal constant
+    {
+        ++end;
+        while (isoctdigit(*end))
+            ++end;
+    }
+    else if (*end == '0' && tolower(*(end + 1)) == 'x') // hexadecimal constant
+    {
+        ++end;
+
+        if (!isxdigit(*end))
+            return start;
+        ++end;
+
+        while (isxdigit(*end))
+            ++end;
+    }
+
+    // integer suffix
+    while (tolower(*end) == 'u' || tolower(*end) == 'l')
+        ++end;
+
+    return end;
+}
+
+// Stop at the first character after current "escape sequence", or start if not
+// match.
+static inline const char * EscapeSequenceEnd(const char * start) {
+    if (*start != '\\')
+        return start;
+    ++start;
+
+    if (isescape(*start)) // simple escape sequence
+        ++start;
+    else if (isoctdigit(*start)) // octal escape sequence
+    {
+        ++start;
+        if (isoctdigit(*start))
+            ++start;
+        if (isoctdigit(*start))
+            ++start;
+    }
+    else if (*start == 'x') // hexadecimal escape sequence
+    {
+        ++start;
+        while (isxdigit(*start))
+            ++start;
+    }
+    return start;
+}
+
+// Stop at the first character after current "char", or start if not match.
+static inline const char * CharConstEnd(const char * start) {
+    const char * end = start;
+
+    if (*end == 'L')
+        ++end;
+
+    if (*end != '\'')
+        return start;
+    ++end;
+    if (*end == '\'')
+        return start;
+
+    do
+    {
+        if (*end == 0)
+            return start;
+
+        if (*end == '\\') // escape sequence
+        {
+            const char * p = EscapeSequenceEnd(end);
+            if (p == end)
+                return start;
+            end = p;
+        }
+        else // non escape character
+            ++end;
+    } while (*end != '\'');
+    ++end;
+
+    return end;
+}
+
+// Stop at the first character after current "string", or start if not match.
+static inline const char * StringLiteralEnd(const char * start) {
+    const char * end = start;
+
+    if (*end == 'L')
+        ++end;
+
+    if (*end != '"')
+        return start;
+    ++end;
+
+    while (*end != '"')
+    {
+        if (*end == 0)
+            return start;
+
+        if (*end == '\\') // escape sequence
+        {
+            const char * p = EscapeSequenceEnd(end);
+            if (p == end)
+                return start;
+            end = p;
+        }
+        else // non escape character
+            ++end;
+    };
+    ++end;
+
+    return end;
+}
+
+// Stop at the first character after current "operator/punctuator", or start if
+// not match.
+static inline const char * OperatorPunctuatorEnd(const char * start) {
+    switch (*start)
+    {
+        case '(':
+        case ')':
+        case ',':
+        case ':':
+        case ';':
+        case '?':
+        case '[':
+        case ']':
+        case '{':
+        case '}':
+            ++start;
+            break;
+        case '!':
+        case '%':
+        case '*':
+        case '/':
+        case '^':
+        case '~':
+        case '=':
+            ++start;
+            if (*start == '=')
+                ++start;
+            break;
+        case '+':
+            ++start;
+            if (*start == '=' || *start == '+')
+                ++start;
+            break;
+        case '&':
+            ++start;
+            if (*start == '=' || *start == '&')
+                ++start;
+            break;
+        case '|':
+            ++start;
+            if (*start == '=' || *start == '|')
+                ++start;
+            break;
+        case '.':
+            ++start;
+            if (*start == '.' && *(start + 1) == '.')
+                start += 2;
+            break;
+        case '-':
+            ++start;
+            if (*start == '-' || *start == '=' || *start == '>')
+                ++start;
+            break;
+        case '<':
+            ++start;
+            if (*start == '=')
+                ++start;
+            else if (*start == '<')
+            {
+                ++start;
+                if (*start == '=')
+                    ++start;
+            }
+            break;
+        case '>':
+            ++start;
+            if (*start == '=')
+                ++start;
+            else if (*start == '>')
+            {
+                ++start;
+                if (*start == '=')
+                    ++start;
+            }
+            break;
+        default:
+            break;
+    }
+    return start;
+}
+
+// Stop at the first character after current token, or start if not match.
+static inline const char * TokenEnd(const char * start,
+                                    TokenRecog::Info * info) {
+    const char * end;
+
+    if ((end = IdEnd(start)) > start) // id or keyword
+    {
+        info->category = TokenRecog::ID;
+        return end;
+    }
+    if ((end = FloatConstEnd(start)) > start) // float-constant
+    {
+        info->category = TokenRecog::FLOAT;
+        return end;
+    }
+    if ((end = IntConstEnd(start)) > start) // int-constant
+    {
+        info->category = TokenRecog::INT;
+        return end;
+    }
+    if ((end = CharConstEnd(start)) > start) // char-constant
+    {
+        info->category = TokenRecog::CHAR;
+        return end;
+    }
+    if ((end = StringLiteralEnd(start)) > start) // string
+    {
+        info->category = TokenRecog::STRING;
+        return end;
+    }
+    if ((end = OperatorPunctuatorEnd(start)) > start) // operator or punctuator
+    {
+        info->category = TokenRecog::OP_OR_PUNC;
+        return end;
+    }
+
+    return start;
+}
+
+Token::Type KeywordType(StringRef text) {
+    static std::unordered_map<std::string, Token::Type> keywords = {
+        {"auto", Token::KW_AUTO},         {"break", Token::KW_BREAK},
+        {"case", Token::KW_CASE},         {"char", Token::KW_CHAR},
+        {"const", Token::KW_CONST},       {"continue", Token::KW_CONTINUE},
+        {"default", Token::KW_DEFAULT},   {"do", Token::KW_DO},
+        {"double", Token::KW_DOUBLE},     {"else", Token::KW_ELSE},
+        {"enum", Token::KW_ENUM},         {"extern", Token::KW_EXTERN},
+        {"float", Token::KW_FLOAT},       {"for", Token::KW_FOR},
+        {"goto", Token::KW_GOTO},         {"if", Token::KW_IF},
+        {"int", Token::KW_INT},           {"long", Token::KW_LONG},
+        {"register", Token::KW_REGISTER}, {"return", Token::KW_RETURN},
+        {"short", Token::KW_SHORT},       {"signed", Token::KW_SIGNED},
+        {"sizeof", Token::KW_SIZEOF},     {"static", Token::KW_STATIC},
+        {"struct", Token::KW_STRUCT},     {"switch", Token::KW_SWITCH},
+        {"typedef", Token::KW_TYPEDEF},   {"union", Token::KW_UNION},
+        {"unsigned", Token::KW_UNSIGNED}, {"void", Token::KW_VOID},
+        {"volatile", Token::KW_VOLATILE}, {"while", Token::KW_WHILE},
+    };
+
+    if (keywords.find(text.toString()) != keywords.end())
+        return keywords[text.toString()];
+    else
+        return Token::UNKNOWN;
+}
+
+Token::Type OperatorPunctuatorType(StringRef text) {
+    static std::unordered_map<std::string, Token::Type>
+        operators_and_punctuators = {
+            {"--", Token::OP_DEC},      {"-", Token::OP_SUB},
+            {"-=", Token::SUB_ASSIGN},  {"->", Token::OP_POINTTO},
+            {"!", Token::BOOL_NOT},     {"!=", Token::REL_NE},
+            {"%", Token::OP_MOD},       {"%=", Token::MOD_ASSIGN},
+            {"&", Token::BIT_AND},      {"&&", Token::BOOL_AND},
+            {"&=", Token::AND_ASSIGN},  {"(", Token::LP},
+            {")", Token::RP},           {"*", Token::OP_MUL},
+            {"*=", Token::MUL_ASSIGN},  {",", Token::OP_COMMA},
+            {".", Token::OP_DOT},       {"...", Token::VAR_PARAM},
+            {"/", Token::OP_DIV},       {"/=", Token::DIV_ASSIGN},
+            {":", Token::OP_COLON},     {";", Token::STMT_END},
+            {"?", Token::OP_QMARK},     {"[", Token::LSB},
+            {"]", Token::RSB},          {"^", Token::BIT_XOR},
+            {"^=", Token::XOR_ASSIGN},  {"{", Token::BLK_BEGIN},
+            {"|", Token::BIT_OR},       {"||", Token::BOOL_OR},
+            {"|=", Token::OR_ASSIGN},   {"}", Token::BLK_END},
+            {"~", Token::BIT_NOT},      {"+", Token::OP_ADD},
+            {"++", Token::OP_INC},      {"+=", Token::ADD_ASSIGN},
+            {"<", Token::REL_LT},       {"<<", Token::BIT_SHL},
+            {"<<=", Token::SHL_ASSIGN}, {"<=", Token::REL_LE},
+            {"=", Token::ASSIGN},       {"==", Token::REL_EQ},
+            {">", Token::REL_GT},       {">=", Token::REL_GE},
+            {">>", Token::BIT_SHR},     {">>=", Token::SHR_ASSIGN},
+        };
+    if (operators_and_punctuators.find(text.toString()) !=
+        operators_and_punctuators.end())
+        return operators_and_punctuators[text.toString()];
+    else
+        return Token::UNKNOWN;
+}
+
+static inline double EvalFloat(StringRef text) {
+    // TODO implement EvalFloat()
+    int digits = 0;
+    double factor = 1.0;
+    bool afterDot = false;
+
+    for (char c : text)
+    {
+        if (isdigit(c))
+        {
+            digits = digits * 10 + (c - '0');
+            if (afterDot)
+                factor *= 10.0;
+        }
+        else if (c == '.')
+        {
+            ASSERT(afterDot == false);
+            afterDot = true;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return (double)digits / factor;
+}
+
+static inline int EvalInt(StringRef text) {
+    // TODO implement EvalInt()
+    int i = 0;
+    for (char c : text)
+    {
+        ASSERT(isdigit(c));
+        i = i * 10 + (c - '0');
+    }
+    return i;
+}
+
+static inline int EvalChar(StringRef text) {
+    // TODO implement EvalChar()
+    ASSERT((text.size() == 3 && text[0] == '\'' && text[2] == '\'') ||
+           (text == "'\\n'"));
+    if (text.size() == 3 && text[0] == '\'' && text[2] == '\'')
+        return text[1];
+    else
+        return '\n';
+}
+
+static inline Token RecognizeToken(const char * start,
+                                   const char * end,
+                                   const TokenRecog::Info & info) {
+    Token token;
+
+    token.text = StringRef(start, end - start);
+    token.type = Token::UNKNOWN;
+
+    ASSERT(info.category != TokenRecog::UNKNOWN);
+    switch (info.category)
+    {
+        case TokenRecog::ID:
+            token.type = KeywordType(token.text);
+            if (token.type == Token::UNKNOWN)
+                token.type = Token::ID;
+            break;
+        case TokenRecog::STRING:
+            token.type = Token::STRING;
+            break;
+        case TokenRecog::OP_OR_PUNC:
+            token.type = OperatorPunctuatorType(token.text);
+            break;
+        case TokenRecog::FLOAT:
+            token.type = Token::CONST_FLOAT;
+            token.fval = EvalFloat(token.text);
+            break;
+        case TokenRecog::INT:
+            token.type = Token::CONST_INT;
+            token.ival = EvalInt(token.text);
+            break;
+        case TokenRecog::CHAR:
+            token.type = Token::CONST_CHAR;
+            token.cval = EvalChar(token.text);
+            break;
+        default:
+            break;
+    }
+
+    return token;
+}
+
+std::vector<Token> Tokenize(std::vector<std::string> vStrLines)
+{
+    std::vector<Token> tokens;
+
+    for (auto strLine : vStrLines)
+    {
+        Token token;
+        token.type = Token::LINE_START;
+        tokens.push_back(token);
+
+        const char * start;
+        const char * end = strLine.data();
+        do
+        {
+            TokenRecog::Info info;
+
+            // Tokenize.
+            start = TokenStart(end);
+            if (*start == 0)
+                break;
+            end = TokenEnd(start, &info);
+            // ERROR: invalid character sequence if fail.
+            if (end == start)
+            {
+                throw std::invalid_argument("Unrecognized token.");
+            }
+
+            // Recognize.
+            Token token = RecognizeToken(start, end, info);
+            tokens.push_back(token);
+        } while (true);
+    }
+
+    Token token;
+    token.type = Token::FILE_END;
+    tokens.push_back(token);
+
+    return tokens;
+}
+
--- Test/UnitTest_Preprocess.cpp
+++ Test/UnitTest_Preprocess.cpp
@@ -1,6 +1,7 @@
 #include "../Source/UnitTest/UnitTest.h"
 
 #include "../Source/Preprocess/LineSlicer.h"
+#include "../Source/Preprocess/Tokenizer.h"
 
 TEST(LineSlicer_Simple)
 {
@@ -27,5 +28,43 @@ TEST(LineSlicer_Simple)
     for (size_t i = 0; i < out.size(); ++i)
     {
         EXPECT_EQ(lines[i], out[i]);
+    }
+}
+
+TEST(Tokenizer_Simple)
+{
+    std::vector<std::string> in = {
+        "int a;",
+        "1;",
+    };
+
+    std::vector<Token::Type> out = {
+        Token::LINE_START, Token::KW_INT, Token::ID, Token::STMT_END,
+        Token::LINE_START, Token::CONST_INT, Token::STMT_END,
+        Token::FILE_END,
+    };
+
+    auto tokens = Tokenize(in);
+    EXPECT_EQ(tokens.size(), out.size());
+    for (size_t i = 0; i < out.size(); ++i)
+    {
+        EXPECT_EQ(tokens[i].type, out[i]);
+    }
+}
+
+TEST(Tokenizer_UnknownToken)
+{
+    std::vector<std::string> in = {
+        "#unknown",
+    };
+
+    try
+    {
+        auto tokens = Tokenize(in);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
+        EXPECT_EQ(e.what(), std::string("Unrecognized token."));
     }
 }
