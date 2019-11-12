571739b31028e1b1abf4fdc0c5f0bb3635dedb6e
Yangmu Jiang <celsius.j@gmail.com>
2019-11-11 22:27:43 -0800

    ...

--- Source/Base/Containers.cpp
+++ Source/Base/Containers.cpp
@@ -23,15 +23,15 @@ TEST(Array_API)
 
     // Insert elements at index.
     Array<int> a4;
-    a4.Insert(0, 2); // 2
-    a4.Insert(1, 4); // 2,4
-    a4.Insert(0, 1); // 1,2,4
-    a4.Insert(2, 3); // 1,2,3,4
+    a4.InsertAt(0, 2); // 2
+    a4.InsertAt(1, 4); // 2,4
+    a4.InsertAt(0, 1); // 1,2,4
+    a4.InsertAt(2, 3); // 1,2,3,4
 
     // Remove elements at index.
-    a4.Remove(0); // 2,3,4
-    a4.Remove(1); // 2,4
-    a4.Remove(1); // 2
+    a4.RemoveAt(0); // 2,3,4
+    a4.RemoveAt(1); // 2,4
+    a4.RemoveAt(1); // 2
     a4.RemoveAll();
 
     // Query elements.
@@ -40,7 +40,7 @@ TEST(Array_API)
     a5.At(4) += 1; // 6
     a5.Contains(4); // true
     a5.Contains(5); // false
-    a5.Empty(); // false
+    a5.IsEmpty(); // false
 
     // Enumerate elements.
     Array<int> a6(5, cstyle);
@@ -56,6 +56,50 @@ TEST(Array_API)
         for (int [index, element] : a6.IndexElementPairs())
         for (int [index, element] : a6.IndexElementPairsReserved())
     */
+}
+
+TEST(List_API)
+{
+    // Create an empty list.
+    List<int> l0;
+
+    // Insert elements at boundary.
+    List<int> l1;
+    l1.AddHead(2); // 2
+    l1.AddTail(4); // 2,4
+
+    // Insert elements at position.
+    l1.InsertAfter(l1.FindElementPos(2), 3); // 2,3,4
+    l1.InsertBefore(l1.FindIndexPos(0), 1); // 1,2,3,4
+
+    // Remove elements at position.
+    l1.RemoveAt(l1.FindIndexPos(1)); // 1,3,4
+
+    // Remove elements at boundary.
+    l1.RemoveHead(); // 3,4
+    l1.RemoveTail(); // 4
+    l1.RemoveAll();
+
+    // Query elements.
+    List<int> l2;
+    l2.AddTail(1);
+    l2.AddTail(2);
+    l2.AddTail(3);
+    l2.Count(); // 3
+    l2.Empty(); // false
+    l2.Head(); // 1
+    l2.At(l2.FindIndexPos(1)); // 2
+    l2.Tail(); // 3
+    
+    // Enumerate elements.
+    List<int> l3;
+    l3.AddTail(1);
+    l3.AddTail(2);
+    l3.AddTail(3);
+    for (auto p = l3.HeadPos(); p; p = l3.FindNextPos(p))
+    {
+        l3.At(p);
+    }
 }
 
 #endif
--- Source/Base/Containers.h
+++ Source/Base/Containers.h
@@ -109,13 +109,24 @@ public:
     ~Array();
     
 // Attributes
-    T &         At(int index);
     const T &   At(int index) const;
+    T &         At(int index);
 
+    bool        IsEmpty() const;
+    int         Count() const;
+
+// Operations
     bool        Contains(T element) const;
 
-    bool        Empty() const;
-    int         Count() const;
+    void        Add(T element);
+    void        Append(const Array & other);
+
+    void        InsertAt(int index, T element, int count = 1);
+    void        RemoveAt(int index, int count = 1);
+    void        RemoveAll();
+
+    T &         operator[] (int index);
+    const T &   operator[] (int index) const;
 
     // Iterator.
     using ElementIt = IteratorPair<T *>;
@@ -127,22 +138,14 @@ public:
     ReversedElementIt   ElementsReversed() const;
     ReversedIndexIt     IndexesReversed() const;
 
-// Operations
-    void        Add(T element);
-    void        Append(const Array & other);
-
-    void        Insert(int index, T element, int count = 1);
-    void        Remove(int index, int count = 1);
-
-    void        RemoveAll();
-
-    T &         operator[] (int index);
-    const T &   operator[] (int index) const;
-
 // Overridables
 
 // Implementation
 private:
+    int nCap;
+    int nCount;
+    T * pData;
+
     static T * Alloc(int count)
     {
         ASSERT(count == CeilPowOf2(count));
@@ -185,11 +188,11 @@ private:
     {
         ASSERT(count > 0);
         // [end, end + count) is invalid (uninitialized, or moved away, or destructed)
-        T * newEnd = end + count;
+        T * nNewEnd = end + count;
         while (begin != end)
         {
-            --newEnd, --end;
-            new (newEnd) T(std::move(*end));
+            --nNewEnd, --end;
+            new (nNewEnd) T(std::move(*end));
         }
         // [begin, begin + count) is invalid
     }
@@ -197,122 +200,118 @@ private:
     {
         ASSERT(count > 0);
         // [begin - count, begin) is invalid
-        T * newBegin = begin - count;
+        T * nNewBegin = begin - count;
         while (begin != end)
         {
-            new (newBegin) T(std::move(*begin));
-            ++newBegin, ++begin;
+            new (nNewBegin) T(std::move(*begin));
+            ++nNewBegin, ++begin;
         }
         // [end - count, end) is invalid
     }
-    void Expand(int newMaxCount)
+    void Expand(int nNewMaxCount)
     {
-        newMaxCount = CeilPowOf2(newMaxCount);
-        if (elemMaxCount != newMaxCount)
+        nNewMaxCount = CeilPowOf2(nNewMaxCount);
+        if (nCap != nNewMaxCount)
         {
-            T * newData = Alloc(newMaxCount);
-            if (0 < elemCount)
+            T * pNewData = Alloc(nNewMaxCount);
+            if (0 < nCount)
             {
-                MoveConstruct(newData, elemData, elemCount);
-                Free(elemData);
+                MoveConstruct(pNewData, pData, nCount);
+                Free(pData);
             }
-            elemData = newData;
-            elemMaxCount = newMaxCount;
+            pData = pNewData;
+            nCap = nNewMaxCount;
         }
-        ASSERT(elemData);
+        ASSERT(pData);
     }
-
-    int elemMaxCount;
-    int elemCount;
-    T * elemData;
 };
 
 template <typename T>
 Array<T>::Array()
-    : elemMaxCount(0)
-    , elemCount(0)
-    , elemData(nullptr)
+    : nCap(0)
+    , nCount(0)
+    , pData(nullptr)
 {
 }
 
 template <typename T>
 Array<T>::Array(const Array & other)
-    : elemMaxCount(other.elemMaxCount)
-    , elemCount(other.elemCount)
-    , elemData(nullptr)
+    : nCap(other.nCap)
+    , nCount(other.nCount)
+    , pData(nullptr)
 {
-    if (elemMaxCount > 0)
+    if (nCap > 0)
     {
-        elemData = Alloc(elemMaxCount);
-        CopyConstruct(elemData, other.elemData, elemCount);
+        pData = Alloc(nCap);
+        CopyConstruct(pData, other.pData, nCount);
     }
 }
 
 template <typename T>
 Array<T>::Array(Array && other)
-    : elemMaxCount(other.elemMaxCount)
-    , elemCount(other.elemCount)
-    , elemData(other.elemData)
+    : nCap(other.nCap)
+    , nCount(other.nCount)
+    , pData(other.pData)
 {
-    other.elemMaxCount = 0;
-    other.elemCount = 0;
-    other.elemData = nullptr;
+    other.nCap = 0;
+    other.nCount = 0;
+    other.pData = nullptr;
 }
 
 template <typename T>
 Array<T>::Array(int count, T element)
-    : elemMaxCount(CeilPowOf2(count))
-    , elemCount(count)
-    , elemData(nullptr)
+    : nCap(CeilPowOf2(count))
+    , nCount(count)
+    , pData(nullptr)
 {
-    if (elemMaxCount > 0)
+    if (nCap > 0)
     {
-        elemData = Alloc(elemMaxCount);
-        CopyConstruct(elemData, element, elemCount);
+        pData = Alloc(nCap);
+        CopyConstruct(pData, element, nCount);
     }
 }
 
 template <typename T>
 Array<T>::Array(int count, const T * elementPtr)
-    : elemMaxCount(CeilPowOf2(count))
-    , elemCount(count)
-    , elemData(nullptr)
+    : nCap(CeilPowOf2(count))
+    , nCount(count)
+    , pData(nullptr)
 {
-    if (elemMaxCount > 0)
+    if (nCap > 0)
     {
-        elemData = Alloc(elemMaxCount);
-        CopyConstruct(elemData, elementPtr, elemCount);
+        pData = Alloc(nCap);
+        CopyConstruct(pData, elementPtr, nCount);
     }
 }
 
 template <typename T>
 Array<T>::~Array()
 {
-    if (elemData)
+    if (pData)
     {
-        Free(elemData);
+        Free(pData);
     }
 }
 
 template <typename T>
 T & Array<T>::At(int index)
 {
-    ASSERT(0 <= index && index < elemCount);
-    return elemData[index];
+    ASSERT(0 <= index && index < nCount);
+    return pData[index];
 }
 
 template <typename T>
 const T & Array<T>::At(int index) const
 {
-    ASSERT(0 <= index && index < elemCount);
-    return elemData[index];
+    ASSERT(0 <= index && index < nCount);
+    return pData[index];
 }
 
 template <typename T>
 bool Array<T>::Contains(T element) const
 {
-    for (T * elemIt = elemData;
-         elemIt != elemData + elemCount;
+    for (T * elemIt = pData;
+         elemIt != pData + nCount;
          ++elemIt)
     {
         if (*elemIt == element)
@@ -322,101 +321,497 @@ bool Array<T>::Contains(T element) const
 }
 
 template <typename T>
-bool Array<T>::Empty() const
+bool Array<T>::IsEmpty() const
 {
-    return elemCount == 0;
+    return nCount == 0;
 }
 
 template <typename T>
 int Array<T>::Count() const
 {
-    return elemCount;
+    return nCount;
 }
 
 template <typename T>
 typename Array<T>::ElementIt Array<T>::Elements() const
 {
-    return Empty() ? ElementIt() : ElementIt(elemData, elemData + elemCount);
+    return IsEmpty() ? ElementIt() : ElementIt(pData, pData + nCount);
 }
 
 template <typename T>
 typename Array<T>::IndexIt Array<T>::Indexes() const
 {
-    return Empty() ? IndexIt() : IndexIt(0, elemCount);
+    return IsEmpty() ? IndexIt() : IndexIt(0, nCount);
 }
 
 template <typename T>
 typename Array<T>::ReversedElementIt Array<T>::ElementsReversed() const
 {
-    return Empty() ? ReversedElementIt() : ReversedElementIt(elemData + elemCount, elemData);
+    return IsEmpty() ? ReversedElementIt() : ReversedElementIt(pData + nCount, pData);
 }
 
 template <typename T>
 typename Array<T>::ReversedIndexIt Array<T>::IndexesReversed() const
 {
-    return Empty() ? ReversedIndexIt() : ReversedIndexIt(elemCount, 0);
+    return IsEmpty() ? ReversedIndexIt() : ReversedIndexIt(nCount, 0);
 }
 
 template <typename T>
 void Array<T>::Add(T element)
 {
-    Expand(elemCount + 1);
+    Expand(nCount + 1);
 
-    CopyConstruct(elemData + elemCount, element, 1);
-    ++elemCount;
+    CopyConstruct(pData + nCount, element, 1);
+    ++nCount;
 }
 
 template <typename T>
 void Array<T>::Append(const Array & other)
 {
-    Expand(elemCount + other.elemCount);
+    Expand(nCount + other.nCount);
 
-    CopyConstruct(elemData + elemCount, other.elemData, other.elemCount);
-    elemCount += other.elemCount;
+    CopyConstruct(pData + nCount, other.pData, other.nCount);
+    nCount += other.nCount;
 }
 
 template <typename T>
-void Array<T>::Insert(int index, T element, int count)
+void Array<T>::InsertAt(int index, T element, int count)
 {
-    ASSERT(0 <= index && index <= elemCount);
+    ASSERT(0 <= index && index <= nCount);
     ASSERT(0 < count);
-    Expand(elemCount + count);
+    Expand(nCount + count);
 
-    ShiftRight(elemData + index, elemData + elemCount, count);
-    CopyConstruct(elemData + index, element, count);
-    elemCount += count;
+    ShiftRight(pData + index, pData + nCount, count);
+    CopyConstruct(pData + index, element, count);
+    nCount += count;
 }
 
 template <typename T>
-void Array<T>::Remove(int index, int count)
+void Array<T>::RemoveAt(int index, int count)
 {
-    ASSERT(0 <= index && index < elemCount);
-    ASSERT(0 < count && count <= (elemCount - index));
+    ASSERT(0 <= index && index < nCount);
+    ASSERT(0 < count && count <= (nCount - index));
 
-    ShiftLeft(elemData + index + count, elemData + elemCount, count);
-    elemCount -= count;
+    ShiftLeft(pData + index + count, pData + nCount, count);
+    nCount -= count;
 }
 
 template <typename T>
 void Array<T>::RemoveAll()
 {
-    if (0 < elemCount)
+    if (0 < nCount)
     {
-        Destruct(elemData, elemCount);
-        elemCount = 0;
+        Destruct(pData, nCount);
+        nCount = 0;
     }
 }
 
 template <typename T>
 T & Array<T>::operator[] (int index)
 {
-    ASSERT(0 <= index && index < elemCount);
-    return elemData[index];
+    ASSERT(0 <= index && index < nCount);
+    return pData[index];
 }
 
 template <typename T>
 const T & Array<T>::operator[] (int index) const
 {
-    ASSERT(0 <= index && index < elemCount);
-    return elemData[index];
+    ASSERT(0 <= index && index < nCount);
+    return pData[index];
+}
+
+// Index Type: int
+// Element Arg Type: T
+// Element Ref Type: T &
+template <typename T>
+class List
+{
+public:
+    using Position = void *;
+
+// Constructors
+	List();
+	List(const List & other);
+	List(List && other);
+	
+// Attributes
+    int Count();
+    bool Empty();
+
+    T & Head();
+    T & Tail();
+    T & At(Position pos);
+
+    Position HeadPos();
+    Position TailPos();
+
+// Operations
+    void AddHead(T element);
+    void AddTail(T element); // Add
+
+    Position FindElementPos(T element);
+    Position FindIndexPos(int index);
+    Position FindNextPos(Position pos);
+    Position FindPrevPos(Position pos);
+
+    void InsertAfter(Position pos, T element);
+    void InsertBefore(Position pos, T element);
+    void RemoveHead();
+    void RemoveTail();
+    void RemoveAt(Position pos);
+    void RemoveAll();
+
+// Implementation
+private:
+    struct Node
+    {
+        Node * pPrev;
+        Node * pNext;
+        T value;
+    };
+
+    int nCount;
+    Node origin; // T supports default construct
+
+    //static inline void  _Init(List & list);
+    static inline Node* _Alloc()
+    {
+        Node * pNode = LowLevel::Alloc(sizeof(Node));
+        pNode->pPrev = nullptr;
+        pNode->pNext = nullptr;
+        return pNode;
+    }
+    static inline void  _Free(Node * pNode)
+    {
+        ASSERT(pNode);
+        LowLevel::Free(pNode);
+    }
+    static inline void  _Link(Node * pLeft, Node * pRight)
+    {
+        ASSERT(pLeft && pRight);
+        ASSERT(!pLeft->pNext && !pRight->pPrev);
+        pLeft->pNext = pRight;
+        pRight->pPrev = pLeft;
+    }
+    static inline void  _Unlink(Node * pLeft, Node * pRight)
+    {
+        ASSERT(pLeft && pRight);
+        ASSERT(pLeft->pNext == pRight && pRight->pPrev == pLeft);
+        pLeft->pNext = nullptr;
+        pRight->pPrev = nullptr;
+    }
+    static inline void  _Construct(Node * pNode, T element)
+    {
+        ASSERT(pNode);
+        new (pNode->value) T(element);
+    }
+    static inline void  _Destruct(Node * pNode)
+    {
+        ASSERT(pNode);
+        pNode->value->~T();
+    }
+    static inline void  _InsertEmpty(List & list, Node * pNew)
+    {
+        ASSERT(list.Empty() && pNew);
+        list.pHead = pNew;
+        list.pTail = pNew;
+        list.nCount = 1;
+    }
+    static inline void  _InsertHead(List & list, Node * pNew)
+    {
+        ASSERT(list.Empty() && pNew);
+        _Link(list.pHead, pNew);
+        list.pHead = pNew;
+        ++list.nCount;
+    }
+    static inline void  _InsertTail(List & list, Node * pNew)
+    {
+        ASSERT(list.Empty() && pNew);
+        _Link(pNew, list.pTail);
+        list.pTail = pNew;
+        ++list.nCount;
+    }
+    static inline void  _InsertBefore(List & list, Node * pPos, Node * pNew)
+    {
+        ASSERT(pPos && pNew);
+        
+    }
+    static inline void  _InsertAfter(List & list, Node * pPos, Node * pNew)
+    {
+        ASSERT(pPos && pNew);
+    }
+};
+
+template <typename T>
+List<T>::List()
+    : nCount(0)
+    , pHead(nullptr)
+    , pTail(nullptr)
+{
+}
+
+template <typename T>
+List<T>::List(List && other)
+    : nCount(other.nCount)
+    , pHead(other.pHead)
+    , pTail(other.pTail)
+{
+    other.nCount = 0;
+    other.pHead = other.pTail = nullptr;
+}
+
+template <typename T>
+List<T>::List(const List & other)
+    : nCount(0)
+    , pHead(nullptr)
+    , pTail(nullptr)
+{
+    Node * pNode;
+    for (pNode = other.pHead;
+         pNode;
+         pNode = pNode->pNext)
+    {
+        AddTail(pNode->value);
+    }
+}
+
+template <typename T>
+int List<T>::Count()
+{
+    return nCount;
+}
+
+template <typename T>
+bool List<T>::Empty()
+{
+    return nCount == 0;
+}
+
+template <typename T>
+T & List<T>::Head()
+{
+    ASSERT(!Empty());
+    return pHead->value;
+}
+
+template <typename T>
+T & List<T>::Tail()
+{
+    ASSERT(!Empty());
+    return pTail->value;
+}
+
+template <typename T>
+T & List<T>::At(Position pos)
+{
+    ASSERT(pos);
+    return static_cast<Node *>(pos)->value;
+}
+
+template <typename T>
+typename List<T>::Position List<T>::HeadPos()
+{
+    ASSERT(!Empty());
+    return pHead;
+}
+
+template <typename T>
+typename List<T>::Position List<T>::TailPos()
+{
+    ASSERT(!Empty());
+    return pTail;
+}
+
+template <typename T>
+void List<T>::AddHead(T element)
+{
+    Node * pNode = _Alloc();
+    _Construct(pNode, element);
+
+    if (Empty())
+        _InsertEmpty(*this, pNode);
+    else
+        _InsertBefore(*this, pHead, pNode);
+}
+
+template <typename T>
+void List<T>::AddTail(T element)
+{
+    Node * pNode = _Alloc();
+    _Construct(pNode, element);
+
+    if (Empty())
+        _InsertEmpty(*this, pNode);
+    else
+        _InsertAfter(*this, pTail, pNode);
+}
+
+template <typename T>
+typename List<T>::Position List<T>::FindElementPos(T element)
+{
+    if (Empty())
+        return nullptr;
+
+    Node * pNode;
+    for (pNode = pHead;
+         pNode != pTail;
+         pNode = pNode->pNext)
+    {
+        if (pNode->value == element)
+            return pNode;
+    }
+    return nullptr;
+}
+
+template <typename T>
+typename List<T>::Position List<T>::FindIndexPos(int index)
+{
+    ASSERT(0 <= index && index < Count());
+
+    if (Empty())
+        return nullptr;
+
+    Node * pNode;
+    for (pNode = pHead;
+         pNode != pTail && 0 < index;
+         pNode = pNode->pNext, --index)
+    {}
+    return pNode;
+}
+
+template <typename T>
+typename List<T>::Position List<T>::FindNextPos(List<T>::Position pos)
+{
+    ASSERT(pos);
+    Node * pNode = static_cast<Node *>(pos);
+
+    return pNode->pNext;
+}
+
+template <typename T>
+typename List<T>::Position List<T>::FindPrevPos(List<T>::Position pos)
+{
+    ASSERT(pos);
+    Node * pNode = static_cast<Node *>(pos);
+
+    return pNode->pPrev;
+}
+
+template <typename T>
+void List<T>::InsertAfter(Position pos, T element)
+{
+    ASSERT(pos);
+    Node * pNode = static_cast<Node *>(pos);
+
+    Node * pNew = _Alloc();
+    _Construct(pNew, element);
+
+    if (pNode == pTail)
+        _InsertTail(*this, pNew);
+    else
+        _InsertAfter(*this, pNode, pNew);
+}
+
+template <typename T>
+void List<T>::InsertBefore(Position pos, T element)
+{
+    ASSERT(pos);
+    Node * pNode = static_cast<Node *>(pos);
+
+    Node * pNew = _Alloc();
+    _Construct(pNew, element);
+
+    if (pNode == pHead)
+        _InsertHead(*this, pNew);
+    else
+        _InsertBefore(*this, pNode, pNew);
+}
+
+template <typename T>
+void List<T>::RemoveHead()
+{
+    ASSERT(!Empty());
+
+    if (Count() == 1)
+    {
+        _Destruct(pHead);
+        _Free(pHead);
+        pHead = pTail = nullptr;
+        nCount = 0;
+    }
+    else
+    {
+        Node * pNode = pHead;
+        pHead = pHead->pNext;
+        _Unlink(pNode, pHead);
+        _Destruct(pNode);
+        _Free(pNode);
+        --nCount;
+    }
+}
+
+template <typename T>
+void List<T>::RemoveTail()
+{
+    ASSERT(!Empty());
+
+    if (Count() == 1)
+    {
+        _Destruct(pHead);
+        _Free(pHead);
+        pHead = pTail = nullptr;
+        nCount = 0;
+    }
+    else
+    {
+        Node * pNode = pTail;
+        pTail = pTail->pPrev;
+        _Unlink(pTail, pNode);
+        _Destruct(pNode);
+        _Free(pNode);
+        --nCount;
+    }
+}
+
+template <typename T>
+void List<T>::RemoveAt(Position pos)
+{
+    ASSERT(pos);
+    Node * pNode = static_cast<Node *>(pos);
+
+    Node * pLeft = pNode->pPrev;
+    Node * pRight = pNode->pNext;
+    
+    if (pLeft)
+        _Unlink(pLeft, pNode);
+    else
+        pHead = pRight;
+
+    if (pRight)
+        _Unlink(pNode, pRight);
+    else
+        pTail = pLeft;
+
+    if (pLeft && pRight)
+        _Link(pLeft, pRight);
+
+    _Destruct(pNode);
+    _Free(pNode);
+    --nCount;
+}
+
+template <typename T>
+void List<T>::RemoveAll()
+{
+    Node * pCur;
+    Node * pNxt = pHead;
+    while (pNxt)
+    {
+        pCur = pNxt;
+        pNxt = pNxt->pNext;
+        _Destruct(pCur);
+        _Free(pCur);
+    }
+    pHead = pTail = nullptr;
+    nCount = 0;
 }
