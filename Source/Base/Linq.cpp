4c2b63697792ce77f09dd50e1610c760aedb99fe
Yangmu Jiang <celsius.j@gmail.com>
2019-11-26 03:45:24 -0800

    ...

--- Source/Base/Linq.cpp
+++ Source/Base/Linq.cpp
@@ -23,11 +23,17 @@ std::ostream & operator << (std::ostream & o, const std::vector<int> & v)
 
 TEST(LINQ_API)
 {
-    std::vector<int> ints = {1, 2, 3, 4, 5};
+    std::vector<int> ints = {2, 4, 5, 1, 3};
 
     std::function<int(char)> f;
 
-    auto ord    = ints | linq::order_by_func<int, int>([](int i){return -i;});
+    auto ord    = ints | linq::order_by([](int i){return -i;});
+    auto rev    = ints | linq::reverse();
+
+    auto ski    = ints | linq::skip(3);
+    auto skw    = ints | linq::skip_while([](int i){return i != 5;});
+    auto tak    = ints | linq::take(2);
+    auto taw    = ints | linq::take_while([](int i){return i != 1;});
 
     int max_int = ints | linq::max();
     int min_int = ints | linq::min();
@@ -39,6 +45,11 @@ TEST(LINQ_API)
         << "origin: " << ints << std::endl
         << std::endl
         << "ord: " << ord << std::endl
+        << "rev: " << rev << std::endl
+        << "ski: " << ski << std::endl
+        << "skw: " << skw << std::endl
+        << "tak: " << tak << std::endl
+        << "taw: " << taw << std::endl
         << "max: " << max_int << std::endl
         << "min: " << min_int << std::endl
         << "cnt: " << cnt << std::endl
--- Source/Base/Linq.h
+++ Source/Base/Linq.h
@@ -2,11 +2,70 @@
 
 #include <functional>
 #include <algorithm>
+#include <tuple>
 
 namespace linq {
 
 // Excludes T from argument deduction.
 template <typename T> struct no_arg_deduction { typedef T type; };
+
+// as seen on http://functionalcpp.wordpress.com/2013/08/05/function-traits/
+template<class F>
+struct function_traits;
+
+// function pointer
+template<class R, class... Args>
+struct function_traits<R(*)(Args...)> : public function_traits<R(Args...)>
+{};
+
+template<class R, class... Args>
+struct function_traits<R(Args...)>
+{
+    using return_type = R;
+
+    static constexpr std::size_t arity = sizeof...(Args);
+
+    template <std::size_t N>
+    struct argument
+    {
+        static_assert(N < arity, "error: invalid parameter index.");
+        using type = typename std::tuple_element<N, std::tuple<Args...>>::type;
+    };
+};
+
+// member function pointer
+template<class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...)> : public function_traits<R(C&, Args...)>
+{};
+
+// const member function pointer
+template<class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...) const> : public function_traits<R(C&, Args...)>
+{};
+
+// member object pointer
+template<class C, class R>
+struct function_traits<R(C::*)> : public function_traits<R(C&)>
+{};
+
+// functor
+template<class F>
+struct function_traits
+{
+private:
+    using call_type = function_traits<decltype(&F::operator())>;
+public:
+    using return_type = typename call_type::return_type;
+
+    static constexpr std::size_t arity = call_type::arity - 1;
+
+    template <std::size_t N>
+    struct argument
+    {
+        static_assert(N < arity, "error: invalid parameter index.");
+        using type = typename call_type::template argument<N + 1>::type;
+    };
+};
 
 // ================================================
 // Sorting
@@ -16,18 +75,75 @@ template <typename T> struct no_arg_deduction { typedef T type; };
 //
 
 template <typename TElement, typename TKey>
-struct order_by
+struct order_by_functor
 {
-    order_by(std::function<TKey(TElement)> pred) : pred(pred) {}
+    order_by_functor(std::function<TKey(TElement)> pred) : pred(pred) {}
 
     std::function<TKey(TElement)> pred;
 };
 
-template <typename TElement, typename TKey>
+template <typename TPred>
 auto
-order_by_func(std::function<TKey(TElement)> pred)
+order_by(TPred pred)
 {
-    return order_by<TElement, TKey>(pred);
+    using TKey = function_traits<TPred>::return_type;
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return order_by_functor<TElement, TKey>(pred);
+}
+
+struct reverse {};
+
+// ================================================
+// Partitioning
+//
+//  linq::skip(n)
+//  linq::skip_while(pred)
+//  linq::take(n)
+//  linq::take_while(pred)
+//
+
+struct skip
+{
+    skip(int n) : n(n) {}
+
+    int n;
+};
+
+template <typename TElement>
+struct skip_while_functor
+{
+    skip_while_functor(std::function<bool(TElement)> pred) : pred(pred) {}
+
+    std::function<bool(TElement)> pred;
+};
+template <typename TPred>
+auto
+skip_while(TPred pred)
+{
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return skip_while_functor<TElement>(pred);
+}
+
+struct take
+{
+    take(int n) : n(n) {}
+
+    int n;
+};
+
+template <typename TElement>
+struct take_while_functor
+{
+    take_while_functor(std::function<bool(TElement)> pred) : pred(pred) {}
+
+    std::function<bool(TElement)> pred;
+};
+template <typename TPred>
+auto
+take_while(TPred pred)
+{
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return take_while_functor<TElement>(pred);
 }
 
 // ================================================
@@ -41,9 +157,9 @@ order_by_func(std::function<TKey(TElement)> pred)
 //
 
 template <typename T>
-struct aggregator
+struct aggregate_functor
 {
-    aggregator(T seed, std::function<T(T, T)> pred) : result(seed), pred(pred) {}
+    aggregate_functor(T seed, std::function<T(T, T)> pred) : result(seed), pred(pred) {}
     void add(T next) { result = pred(result, next); }
     T get() { return result; }
     T result;
@@ -54,7 +170,7 @@ auto
 aggregate(TElement seed,
           typename no_arg_deduction<std::function<TElement(TElement, TElement)>>::type pred)
 {
-    return aggregator<TElement>(seed, pred);
+    return aggregate_functor<TElement>(seed, pred);
 }
 
 struct average {};
@@ -64,8 +180,8 @@ struct min {};
 
 }
 
-template <typename TRange, typename TKey, typename TElement = TRange::value_type>
-TRange operator | (const TRange & source, linq::order_by<TElement, TKey> ord)
+template <typename TRange, typename TElement = TRange::value_type, typename TKey>
+TRange operator | (const TRange & source, linq::order_by_functor<TElement, TKey> ord)
 {
     TRange source_copy(source);
     std::sort(source_copy.begin(),
@@ -78,7 +194,51 @@ TRange operator | (const TRange & source, linq::order_by<TElement, TKey> ord)
 }
 
 template <typename TRange, typename TElement = TRange::value_type>
-TElement operator | (const TRange & source, linq::aggregator<TElement> agg)
+TRange operator | (const TRange & source, linq::reverse)
+{
+    return TRange(source.rbegin(), source.rend());
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+TRange operator | (const TRange & source, linq::skip ski)
+{
+    int n = ski.n;
+    auto it = source.begin();
+    while (it != source.end() && 0 < n)
+        ++it, --n;
+    return TRange(it, source.end());
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+TRange operator | (const TRange & source, linq::skip_while_functor<TElement> ski)
+{
+    auto it = source.begin();
+    while (it != source.end() && ski.pred(*it))
+        ++it;
+    return TRange(it, source.end());
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+TRange operator | (const TRange & source, linq::take tak)
+{
+    int n = tak.n;
+    auto it = source.begin();
+    while (it != source.end() && 0 < n)
+        ++it, --n;
+    return TRange(source.begin(), it);
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+TRange operator | (const TRange & source, linq::take_while_functor<TElement> tak)
+{
+    auto it = source.begin();
+    while (it != source.end() && tak.pred(*it))
+        ++it;
+    return TRange(source.begin(), it);
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+TElement operator | (const TRange & source, linq::aggregate_functor<TElement> agg)
 {
     for (const TElement & elem : source)
     {
