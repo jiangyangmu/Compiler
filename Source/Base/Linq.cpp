6d5c375300d7afc5cb236d66e98bccc73483feaf
Yangmu Jiang <celsius.j@gmail.com>
2019-12-02 23:16:42 -0800

    ...

--- Source/Base/Linq.cpp
+++ Source/Base/Linq.cpp
@@ -4,9 +4,11 @@
 #include "../UnitTest/UnitTest.h"
 
 #include <vector>
-#include <complex>
+#include <set>
+#include <iomanip>
 
-std::ostream & operator << (std::ostream & o, const std::vector<int> & v)
+template <typename TContainer>
+std::ostream & OutputContainer(std::ostream & o, const TContainer & v)
 {
     o << "{";
     if (!v.empty())
@@ -20,38 +22,147 @@ std::ostream & operator << (std::ostream & o, const std::vector<int> & v)
     o << "}";
     return o;
 }
+template <template<typename, typename> typename TContainer, typename TElement, typename A>
+std::ostream & operator<< (std::ostream & o, const TContainer<TElement, A> & v)
+{
+    return OutputContainer<TContainer<TElement, A>>(o, v);
+}
+template <template<typename, typename, typename> typename TContainer, typename TElement, typename A, typename B>
+std::ostream & operator<< (std::ostream & o, const TContainer<TElement, A, B> & v)
+{
+    return OutputContainer<TContainer<TElement, A, B>>(o, v);
+}
+
+template <typename A, typename B>
+std::ostream & operator<< (std::ostream & o, const std::pair<A, B> & p)
+{
+    return o << "(" << p.first << ", " << p.second << ")";
+}
+template <typename K, typename V>
+std::ostream & operator<< (std::ostream & o, const std::map<K, V> & m)
+{
+    o << "{";
+    if (!m.empty())
+    {
+        o << " ";
+        auto it = m.begin();
+        o << it->first << ":" << it->second;
+        while (++it != m.end()) o << ", " << it->first << ":" << it->second;
+        o << " ";
+    }
+    o << "}";
+    return o;
+}
+
+struct IdAndName {
+    int id;
+    std::string name;
+
+    friend std::ostream & operator<< (std::ostream & o, const IdAndName & name)
+    {
+        return o << name.name.c_str();
+    }
+};
+struct IdAndMajor {
+    int id;
+    std::string major;
+
+    friend std::ostream & operator<< (std::ostream & o, const IdAndMajor & major)
+    {
+        return o << major.major.c_str();
+    }
+};
 
 TEST(LINQ_API)
 {
-    std::vector<int> ints = {2, 4, 5, 1, 3};
+    std::vector<int> ints = {2, 4, 4, 5, 3, 1, 3};
 
-    std::function<int(char)> f;
+    auto filt   = ints | linq::where([](int i){ return i % 2 == 0; });
+
+    auto all    = ints | linq::all([](int i){ return i < 5; });
+    auto any    = ints | linq::any([](int i){ return i < 5; });
+
+    struct Point { int x; float y; };
+    std::vector<Point> pts = { {1, 2.0f}, {2, 3.0f}, {3, 4.0f} };
+    auto sel_x  = pts | linq::select([](const Point & p) { return p.x; });
+    auto sel_y  = pts | linq::select([](const Point & p) { return p.y; });
+    auto sel_xx = pts | linq::select_many([](const Point & p) { std::vector<int> v = { p.x, p.x }; return v; });
 
     auto ord    = ints | linq::order_by([](int i){return -i;});
     auto rev    = ints | linq::reverse();
+
+    std::set<int> ints_expt = { 1, 2, 3 };
+    std::set<int> ints_itst = { 4, 5, 6, 7 };
+    std::set<int> ints_unio = { 4, 5, 6, 7 };
+    auto dist   = ints | linq::distinct();
+    auto expt   = ints | linq::except(ints_expt);
+    auto itst   = ints | linq::intersect(ints_itst);
+    auto unio   = ints | linq::union_(ints_unio);
 
     auto ski    = ints | linq::skip(3);
     auto skw    = ints | linq::skip_while([](int i){return i != 5;});
     auto tak    = ints | linq::take(2);
     auto taw    = ints | linq::take_while([](int i){return i != 1;});
 
+   
+    std::vector<IdAndName> names = {
+        { 1, "Peter" },
+        { 2, "Ann" },
+        { 4, "John" },
+    };
+    std::vector<IdAndMajor> majors = {
+        { 2, "Economics" },
+        { 3, "Politics" },
+        { 2, "Psychology" },
+        { 1, "Computer Science" },
+    };
+    auto joi    = names | linq::join(majors,
+                                     [](const IdAndName & name) { return name.id; },
+                                     [](const IdAndMajor & major) { return major.id; });
+    auto gjoi   = names | linq::group_join(majors,
+                                     [](const IdAndName & name) { return name.id; },
+                                     [](const IdAndMajor & major) { return major.id; });
+
     int max_int = ints | linq::max();
     int min_int = ints | linq::min();
+    int sum     = ints | linq::sum();
     int cnt     = ints | linq::count();
     int avg     = ints | linq::average();
     int mul     = ints | linq::aggregate(1, [](int x, int y) -> int { return x * y; });
 
     std::cout
+        << std::fixed << std::setprecision(2)
         << "origin: " << ints << std::endl
         << std::endl
+
+        << "filt: " << filt << std::endl
+
+        << "all: " << all << std::endl
+        << "any: " << any << std::endl
+
+        << "sel_x: " << sel_x << std::endl
+        << "sel_y: " << sel_y << std::endl
+        << "sel_xx: " << sel_xx << std::endl
+
         << "ord: " << ord << std::endl
         << "rev: " << rev << std::endl
+
+        << "dist: " << dist << std::endl
+        << "expt" << ints_expt << ": " << expt << std::endl
+        << "itst" << ints_itst << ": " << itst << std::endl
+        << "unio" << ints_unio << ": " << unio << std::endl
+
         << "ski: " << ski << std::endl
         << "skw: " << skw << std::endl
         << "tak: " << tak << std::endl
         << "taw: " << taw << std::endl
+
+        << "joi: " << joi << std::endl
+        << "gjoi: " << gjoi << std::endl
+
         << "max: " << max_int << std::endl
         << "min: " << min_int << std::endl
+        << "sum: " << sum << std::endl
         << "cnt: " << cnt << std::endl
         << "avg: " << avg << std::endl
         << "mul: " << mul << std::endl;
--- Source/Base/Linq.h
+++ Source/Base/Linq.h
@@ -3,6 +3,9 @@
 #include <functional>
 #include <algorithm>
 #include <tuple>
+#include <set>
+#include <map>
+#include <iterator>
 
 namespace linq {
 
@@ -68,6 +71,109 @@ public:
 };
 
 // ================================================
+// Filter
+//
+//  linq::where(pred)
+//
+
+template <typename TElement>
+struct where_functor
+{
+    where_functor(std::function<bool(TElement)> pred) : pred(pred) {}
+
+    std::function<bool(TElement)> pred;
+};
+
+template <typename TPred>
+auto
+where(TPred pred)
+{
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return where_functor<TElement>(pred);
+}
+
+// ================================================
+// Qualifier
+//
+//  linq::all(pred)
+//  linq::any(pred)
+//
+
+template <typename TElement>
+struct all_functor
+{
+    all_functor(std::function<bool(TElement)> pred) : pred(pred) {}
+
+    std::function<bool(TElement)> pred;
+};
+
+template <typename TPred>
+auto
+all(TPred pred)
+{
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return all_functor<TElement>(pred);
+}
+
+template <typename TElement>
+struct any_functor
+{
+    any_functor(std::function<bool(TElement)> pred) : pred(pred) {}
+
+    std::function<bool(TElement)> pred;
+};
+
+template <typename TPred>
+auto
+any(TPred pred)
+{
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return any_functor<TElement>(pred);
+}
+
+// ================================================
+// Projection
+//
+//  linq::select(pred)
+//  linq::select_many(pred)
+//
+
+template <typename TElement, typename TSubElement>
+struct select_functor
+{
+    select_functor(std::function<TSubElement(TElement)> pred) : pred(pred) {}
+
+    std::function<TSubElement(TElement)> pred;
+};
+
+template <typename TPred>
+auto
+select(TPred pred)
+{
+    using TSubElement = function_traits<TPred>::return_type;
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return select_functor<TElement, TSubElement>(pred);
+}
+
+template <typename TElement, typename TSubElementRange>
+struct select_many_functor
+{
+    select_many_functor(std::function<TSubElementRange(TElement)> pred) : pred(pred) {}
+
+    std::function<TSubElementRange(TElement)> pred;
+};
+
+template <typename TPred>
+auto
+select_many(TPred pred)
+{
+    using TSubElementRange = function_traits<TPred>::return_type;
+    using TElement = function_traits<TPred>::argument<0>::type;
+    return select_many_functor<TElement, TSubElementRange>(pred);
+}
+
+
+// ================================================
 // Sorting
 //
 //  linq::order_by(pred)
@@ -92,6 +198,67 @@ order_by(TPred pred)
 }
 
 struct reverse {};
+
+// ================================================
+// Set
+//
+//  linq::distinct()
+//  linq::except(range)
+//  linq::intersect(range)
+//  linq::union(range)
+//
+
+struct distinct {};
+
+template <typename TRange>
+struct range_functor
+{
+    range_functor(const TRange & range) : range(range) {}
+
+    const TRange & range;
+};
+
+template <typename TRange>
+struct except_functor
+{
+    except_functor(const TRange & range) : range(range) {}
+
+    const TRange & range;
+};
+template <typename TRange>
+auto
+except(const TRange & range)
+{
+    return except_functor<TRange>(range);
+};
+
+template <typename TRange>
+struct intersect_functor
+{
+    intersect_functor(const TRange & range) : range(range) {}
+
+    const TRange & range;
+};
+template <typename TRange>
+auto
+intersect(const TRange & range)
+{
+    return intersect_functor<TRange>(range);
+};
+
+template <typename TRange>
+struct union_functor
+{
+    union_functor(const TRange & range) : range(range) {}
+
+    const TRange & range;
+};
+template <typename TRange>
+auto
+union_(const TRange & range)
+{
+    return union_functor<TRange>(range);
+};
 
 // ================================================
 // Partitioning
@@ -147,11 +314,68 @@ take_while(TPred pred)
 }
 
 // ================================================
+// Join
+//
+//  linq::join(range, pred_x, pred_y)
+//
+
+
+template <typename TRange, typename TElementX, typename TElementY, typename TKey>
+struct join_functor
+{
+    join_functor(TRange range,
+                 std::function<TKey(TElementX)> pred_x,
+                 std::function<TKey(TElementY)> pred_y)
+        : range(range)
+        , pred_x(pred_x)
+        , pred_y(pred_y) {}
+
+    TRange range;
+    std::function<TKey(TElementX)> pred_x;
+    std::function<TKey(TElementY)> pred_y;
+};
+template <typename TRange, typename TPredX, typename TPredY>
+auto
+join(TRange range,
+     TPredX pred_x,
+     TPredY pred_y)
+{
+    using TElementX = function_traits<TPredX>::argument<0>::type;
+    using TElementY = function_traits<TPredY>::argument<0>::type;
+    using TKeyX = function_traits<TPredX>::return_type;
+    using TKeyY = function_traits<TPredY>::return_type;
+    static_assert(std::is_same<TKeyX, TKeyY>::value, "Preds must have the same return type.");
+    return join_functor<TRange, TElementX, TElementY, TKeyX>(range, pred_x, pred_y);
+}
+
+template <typename TRange, typename TElementX, typename TElementY, typename TKey>
+struct group_join_functor : public join_functor<TRange, TElementX, TElementY, TKey>
+{
+public:
+    using join_functor<TRange, TElementX, TElementY, TKey>::join_functor;
+};
+template <typename TRange, typename TPredX, typename TPredY>
+auto
+group_join(TRange range,
+           TPredX pred_x,
+           TPredY pred_y)
+{
+    using TElementX = function_traits<TPredX>::argument<0>::type;
+    using TElementY = function_traits<TPredY>::argument<0>::type;
+    using TKeyX = function_traits<TPredX>::return_type;
+    using TKeyY = function_traits<TPredY>::return_type;
+    static_assert(std::is_same<TKeyX, TKeyY>::value, "Preds must have the same return type.");
+    return group_join_functor<TRange, TElementX, TElementY, TKeyX>(range, pred_x, pred_y);
+}
+
+
+// ================================================
 // Aggregation
 //
 //  linq::aggregate(seed, pred)
 //  linq::average()
 //  linq::count()
+//  linq::sum()
 //  linq::max()
 //  linq::min()
 //
@@ -175,9 +399,78 @@ aggregate(TElement seed,
 
 struct average {};
 struct count {};
+struct sum {};
 struct max {};
 struct min {};
 
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+TRange operator | (const TRange & source, linq::where_functor<TElement> whe)
+{
+    TRange source_filtered;
+
+    auto inserter = std::back_inserter(source_filtered);
+    for (auto it = source.begin(); it != source.end(); ++it)
+    {
+        if (whe.pred(*it))
+            inserter = *it;
+    }
+
+    return source_filtered;
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+bool operator | (const TRange & source, linq::all_functor<TElement> all)
+{
+    for (auto it = source.begin(); it != source.end(); ++it)
+    {
+        if (!all.pred(*it))
+            return false;
+    }
+    return true;
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+bool operator | (const TRange & source, linq::any_functor<TElement> any)
+{
+    for (auto it = source.begin(); it != source.end(); ++it)
+    {
+        if (any.pred(*it))
+            return true;
+    }
+    return false;
+}
+
+template <typename TRange, typename TElement = TRange::value_type, typename TSubElement>
+auto operator | (const TRange & source, linq::select_functor<TElement, TSubElement> sel)
+{
+    std::vector<TSubElement> source_selected;
+    
+    source_selected.reserve(source.size());
+    for (auto it = source.begin(); it != source.end(); ++it)
+    {
+        source_selected.push_back(sel.pred(*it));
+    }
+
+    return source_selected;
+}
+
+template <typename TRange, typename TElement = TRange::value_type, typename TSubElementRange>
+auto operator | (const TRange & source, linq::select_many_functor<TElement, TSubElementRange> sel)
+{
+    using TSubElement = TSubElementRange::value_type;
+    std::vector<TSubElement> source_selected;
+
+    for (auto it = source.begin(); it != source.end(); ++it)
+    {
+        auto sub_range = sel.pred(*it);
+        source_selected.insert(source_selected.end(),
+                               sub_range.begin(),
+                               sub_range.end());
+    }
+
+    return source_selected;
 }
 
 template <typename TRange, typename TElement = TRange::value_type, typename TKey>
@@ -197,6 +490,45 @@ template <typename TRange, typename TElement = TRange::value_type>
 TRange operator | (const TRange & source, linq::reverse)
 {
     return TRange(source.rbegin(), source.rend());
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+auto operator | (const TRange & source, linq::distinct)
+{
+    return std::set<TElement>(source.begin(), source.end());
+}
+
+template <typename TRange, typename TElement = TRange::value_type, typename TSetRange>
+auto operator | (const TRange & source, linq::except_functor<TSetRange> sr)
+{
+    std::set<TElement> source_set(source.begin(), source.end());
+    std::set<TElement> result;
+    std::set_difference(source_set.begin(), source_set.end(),
+                        sr.range.begin(), sr.range.end(),
+                        std::inserter(result, result.end()));
+    return result;
+}
+
+template <typename TRange, typename TElement = TRange::value_type, typename TSetRange>
+auto operator | (const TRange & source, linq::intersect_functor<TSetRange> sr)
+{
+    std::set<TElement> source_set(source.begin(), source.end());
+    std::set<TElement> result;
+    std::set_intersection(source_set.begin(), source_set.end(),
+                          sr.range.begin(), sr.range.end(),
+                          std::inserter(result, result.end()));
+    return result;
+}
+
+template <typename TRange, typename TElement = TRange::value_type, typename TSetRange>
+auto operator | (const TRange & source, linq::union_functor<TSetRange> sr)
+{
+    std::set<TElement> source_set(source.begin(), source.end());
+    std::set<TElement> result;
+    std::set_union(source_set.begin(), source_set.end(),
+                   sr.range.begin(), sr.range.end(),
+                   std::inserter(result, result.end()));
+    return result;
 }
 
 template <typename TRange, typename TElement = TRange::value_type>
@@ -235,6 +567,54 @@ TRange operator | (const TRange & source, linq::take_while_functor<TElement> tak
     while (it != source.end() && tak.pred(*it))
         ++it;
     return TRange(source.begin(), it);
+}
+
+template <typename TRangeX, typename TElementX = TRangeX::value_type,
+    typename TRangeY, typename TElementY = TRangeY::value_type, typename TKey>
+auto operator | (const TRangeX & source, linq::join_functor<TRangeY, TElementX, TElementY, TKey> joi)
+{
+    std::vector<std::pair<
+        std::remove_reference<TElementX>::type,
+        std::remove_reference<TElementY>::type>> result;
+    for (auto it_x = source.begin(); it_x != source.end(); ++it_x)
+    {
+        for (auto it_y = joi.range.begin(); it_y != joi.range.end(); ++it_y)
+        {
+            if (joi.pred_x(*it_x) == joi.pred_y(*it_y))
+            {
+                result.emplace_back(*it_x, *it_y);
+            }
+        }
+    }
+    return result;
+}
+
+template <typename TRangeX, typename TElementX = TRangeX::value_type,
+    typename TRangeY, typename TElementY = TRangeY::value_type, typename TKey>
+auto operator | (const TRangeX & source, linq::group_join_functor<TRangeY, TElementX, TElementY, TKey> joi)
+{
+    using TElementXValue = std::remove_const<std::remove_reference<TElementX>::type>::type;
+    using TElementYValue = std::remove_const<std::remove_reference<TElementY>::type>::type;
+
+    std::map<
+        TKey,
+        std::pair<TElementXValue, std::vector<TElementYValue>>
+    > result;
+
+    for (auto it_x = source.begin(); it_x != source.end(); ++it_x)
+    {
+        for (auto it_y = joi.range.begin(); it_y != joi.range.end(); ++it_y)
+        {
+            auto key_x = joi.pred_x(*it_x);
+            if (key_x == joi.pred_y(*it_y))
+            {
+                auto & p = result[key_x];
+                p.first = *it_x;
+                p.second.emplace_back(*it_y);
+            }
+        }
+    }
+    return result;
 }
 
 template <typename TRange, typename TElement = TRange::value_type>
@@ -278,6 +658,22 @@ TElement operator | (const TRange & source, linq::count)
     }
 
     return count;
+}
+
+template <typename TRange, typename TElement = TRange::value_type>
+TElement operator | (const TRange & source, linq::sum)
+{
+    auto it = source.begin();
+    auto end = source.end();
+
+    TElement s = TElement(0);
+    while (it != end)
+    {
+        s += *it;
+        ++it;
+    }
+
+    return s;
 }
 
 template <typename TRange, typename TElement = TRange::value_type>
